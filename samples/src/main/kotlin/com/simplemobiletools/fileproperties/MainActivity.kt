46882284915e0c70fc73bff96a7a912f9e5f7662
tibbi <tibor@kaputa.sk>
2016-10-26 23:43:44 +0200

    show a file picker on button click

--- samples/src/main/kotlin/com/simplemobiletools/fileproperties/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/fileproperties/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.simplemobiletools.fileproperties
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import com.simplemobiletools.filepicker.dialogs.FilePickerDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val STORAGE_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         select_file_button.setOnClickListener {
+            if (hasStoragePermission())
+                showFilePicker()
+            else
+                requestStoragePermission()
+        }
+    }
 
+    private fun showFilePicker() {
+        FilePickerDialog(this, listener = object : FilePickerDialog.OnFilePickerListener {
+            override fun onFail(error: FilePickerDialog.FilePickerResult) {
+            }
+
+            override fun onSuccess(path: String) {
+
+            }
+        })
+    }
+
+    private fun requestStoragePermission() = ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION)
+    private fun hasStoragePermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            showFilePicker()
         }
     }
 }
